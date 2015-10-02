6df9d0445c6543288d887a34552d7406a3d4fdb9
isaacrael <grael@comcast.net>
2015-10-01 22:10:50 -0600

    git commit -m "added registration templatesl"

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'bookstore.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'store.views.index', name='index'),
-    url(r'^store/', 'store.views.store', name='store'),
+
+    url(r'^store/', include('store.urls'), name='store'),
+    url(r'^registration/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
